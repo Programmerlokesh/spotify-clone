759d8626a2a3572c7163ee3a8a3b0cffe41695fc
Lokesh Debnath <lokeshdebnath8@gmail.com>
2022-04-06 22:14:34 +0530

    last commit

--- src/Components/MusicPlayer.js
+++ src/Components/MusicPlayer.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { BsDownload } from "react-icons/bs";
 import {
   FaBackward,
@@ -16,24 +16,71 @@ import "../Styles/MusicPlayer.css";
 function MusicPlayer({ song, imgSrc }) {
   const [isLove, setLove] = useState(false);
   const [isPlaying, setPlaying] = useState(false);
+  const [duration, setDuration] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
 
   const audioPlayer = useRef(); //Our Audio tage
   const progressBar = useRef(); // our Progress bar
+  const animationRef = useRef();
+
+  useEffect(() => {
+    const seconds = Math.floor(audioPlayer.current.duration);
+    setDuration(seconds);
+  }, [audioPlayer?.current?.loadedmetadata, audioPlayer?.current?.readyState]);
+
+  const changePlayPause = () => {
+    setPlaying(!isPlaying);
+    const previousValue = isPlaying;
+    if (!previousValue) {
+      audioPlayer.current.play();
+      animationRef.current = requestAnimationFrame(whilePlaying);
+    } else {
+      audioPlayer.current.pause();
+      cancelAnimationFrame(animationRef.current);
+    }
+    setPlaying(!previousValue);
+  };
+
+  const claculateTime = (sec) => {
+    const minutes = Math.floor(sec / 60);
+    //<10 -> 09 or 11,12
+    const returnMin = minutes < 10 ? `0${minutes}` : `${minutes}`;
+
+    const seconds = Math.floor(sec % 60);
+    const returnSec = seconds < 10 ? `0${seconds}` : `${seconds}`;
+
+    return `${returnMin} : ${returnSec}`;
+  };
+  const changeProgress = () => {
+    audioPlayer.current.currentTime = progressBar.current.value;
+    changeCurrentTime();
+  };
+
+  const whilePlaying = () => {
+    progressBar.current.value = audioPlayer.current.currentTime;
+    changeCurrentTime();
+    animationRef.current = requestAnimationFrame(whilePlaying);
+  };
+
+  const changeCurrentTime = () => {
+    progressBar.current.style.setProperty(
+      "--player-played",
+      `${(progressBar.current.value / duration) * 100}%`
+    );
+    setCurrentTime(progressBar.current.value);
+  };
 
   const chnageLoved = () => {
     setLove(!isLove);
   };
 
-  const changeplay = () => {
-    setPlaying(!isPlaying);
-  };
   return (
     <div className="musicPlayer">
       <div className="songImage">
         <img src={imgSrc} alt="" />
       </div>
       <div className="songAttributes">
-        <audio src={song} preload="metadata" />
+        <audio src={song} preload="metadata" ref={audioPlayer} />
 
         <div className="top">
           <div className="left">
@@ -65,7 +112,7 @@ function MusicPlayer({ song, imgSrc }) {
                 </i>
               </i>
             </div>
-            <div className="playPause" onClick={changeplay}>
+            <div className="playPause" onClick={changePlayPause}>
               {isPlaying ? (
                 <i>
                   <FaPause />
@@ -92,9 +139,18 @@ function MusicPlayer({ song, imgSrc }) {
           </div>
         </div>
         <div className="bottom">
-          <div className="currentTime">00:00</div>
-          <input type="range" className="progresBar" />
-          <div className="duration">00:00</div>
+          <div className="currentTime">{claculateTime(currentTime)}</div>
+          <input
+            type="range"
+            className="progresBar"
+            ref={progressBar}
+            onChange={changeProgress}
+          />
+          <div className="duration">
+            {duration && !isNaN(duration) && claculateTime(duration)
+              ? claculateTime(duration)
+              : "00:00"}
+          </div>
         </div>
       </div>
     </div>
--- src/Components/RightMenu.js
+++ src/Components/RightMenu.js
@@ -1,8 +1,38 @@
 import React from "react";
+import { FaBell, FaCogs, FaCrown, FaRegHeart, FaSun } from "react-icons/fa";
+import Profile from "../img/profile.jpg";
 import "../Styles/RightMenu.css";
 
 function RightMenu() {
-  return <div className="rightMenu"></div>;
+  return (
+    <div className="rightMenu">
+      <div className="goPro">
+        <i>
+          <FaCrown />
+          <p>
+            Go <span>Pro</span>
+          </p>
+        </i>
+        <i>
+          <FaBell />
+        </i>
+        <i>
+          <FaRegHeart />
+        </i>
+      </div>
+      <div className="profile">
+        <i>
+          <FaSun />
+        </i>
+        <i>
+          <FaCogs />
+        </i>
+        <div className="profileImage">
+          <img src={Profile} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export { RightMenu };
